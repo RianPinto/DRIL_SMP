971808801b1dda49dd72354ac7bea4aba0db0433
RianPinto <139946131+RianPinto@users.noreply.github.com>
2023-11-20 19:54:47 +0530

    Update comparator.cpp

--- Week2/comparator.cpp
+++ Week2/comparator.cpp
@@ -4,31 +4,31 @@
 using namespace std;
 
 template <typename T>
-bool areEqual(T a, T b) {
+bool equalcheck(T a, T b) {
     return a == b;
 }
 template <typename T>       // needed for variadic template, sometimes only one number is sent as parameter(when odd number of numbers are compared)
-bool areEqual(T a) {
+bool equalcheck(T a) {
     return 1;
 }
 template <>
-bool areEqual(float a,float b) {
+bool equalcheck(float a,float b) {
     return abs(a - b) < error;
 }
 
 template <>
-bool areEqual(double a, double b) {
+bool equalcheck(double a, double b) {
     return abs(a - b) < error;
 }
 
 template <typename T, typename... Args>
-bool areEqual(T first, T second, Args... args) {
-    return areEqual(first, second) && areEqual(args...);
+bool equalcheck(T first, T second, Args... args) {
+    return equalcheck(first, second) && equalcheck(args...);
 }
 
 int main() {
-    cout << areEqual(1, 1, 1, 1, 1, 1, 1) << endl;
-    cout << areEqual(2.829f, 2.719f) << endl; // error is 0.1, they arent equal
-    cout << areEqual(2.729f, 2.719f) << endl; //withing error range
-    cout << areEqual(2.10, 2.11,2.12); //within error range
+    cout << equalcheck(1, 1, 1, 1, 1, 1, 1) << endl;
+    cout << equalcheck(2.829f, 2.719f) << endl; // error is 0.1, they arent equal
+    cout << equalcheck(2.729f, 2.719f) << endl; //withing error range
+    cout << equalcheck(2.10, 2.11,2.12); //within error range
 }
